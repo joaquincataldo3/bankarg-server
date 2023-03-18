8ed6b6cd88b3dc9152e013b339230e9e479a6d88
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-18 16:26:01 -0400

    api structure

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ require("dotenv").config()
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path')
-const cors = require('cors')
+
 
 const app = express();
 const { MONGO_URI } = process.env
 
-app.use(cors())
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-try {
-    mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log('Mongo DB Connected');
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-            console.log(`Server opened on ${PORT}`);
-        })
-    })
-} catch (error) {
-    console.log(`Mongo DB connection error: ${error}`)
-    process.exit(1)
+
+const connect = async () => {
+    try {
+        await mongoose.connect(MONGO_URI)
+    } catch (error) {
+        console.log(`Mongo DB connection error: ${error}`)
+        process.exit(1)
+    }
 }
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    connect()
+    console.log(`Server opened on ${PORT}`);
+})
+
 
 
 
