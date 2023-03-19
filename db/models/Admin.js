e4fe32b5115cbfbb91674c80941488f659f57154
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-18 21:13:30 -0400

    updating admin model

--- db/models/Admin.js
+++ db/models/Admin.js
@@ -9,10 +9,6 @@ const Admin = new Schema({
         type: String, 
         required: true
     },
-    email: {
-        type: String,
-        required: true
-    },
     createdAt: {
         type: Date,
         default: () => Date.now(), 
--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,3 +1,7 @@
+const Admin = require('../../db/models/Admin')
+const bcryptjs = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+
 const controller = {
     getAll: (req, res) => {
         return res.status(200).json({msg: 'all admins'})
@@ -8,9 +12,20 @@ const controller = {
     processLogin: (req, res) => {
 
     },
-    processRegister: (req, res) => {
+    processRegister: asyncHandler(async (req, res) => {
+        const { username, password } = req.body
 
-    },
+        if (!username || !password ) {
+            res.status(400)
+            throw new Error('Please complete all the fields')
+         }
+
+        const hashPassword = bcryptjs.hashSync(password, 10)
+
+        const newAdmin = await Admin.create({ username, password: hashPassword })
+
+        return res.status(201).json(newAdmin);
+    }),
     updateOne: (req, res) => {
 
     },
